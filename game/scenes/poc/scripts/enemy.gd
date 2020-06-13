47006a942ec972d25edb4f3471704f4d76197e55
Oskar van Velden <oskar@rakso.nl>
2020-06-13 14:45:17 +0200

    Remove debug logs

--- game/scenes/poc/scripts/enemy.gd
+++ game/scenes/poc/scripts/enemy.gd
@@ -15,17 +15,13 @@ export(LookDirection) var look_direction = LookDirection.RIGHT
 func _ready():
 	match look_direction:
 		LookDirection.LEFT:
-			print("look left, bro")
 			vision_cone.rotation_degrees = 180
 
 		LookDirection.UP:
-			print("look up, mistah")
 			vision_cone.rotation_degrees = -90
 
 		LookDirection.RIGHT:
-			print("look to the right, mate")
 			vision_cone.rotation_degrees = 0
 
 		LookDirection.DOWN:
-			print("look down on me, sir")
 			vision_cone.rotation_degrees = 90
