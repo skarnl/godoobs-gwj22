51b892d1c94994fa590c91e67ec9be350b290bc7
Oskar van Velden <oskar@rakso.nl>
2020-06-13 14:32:48 +0200

    Rotate vision cone

--- game/scenes/poc/scripts/vision_cone.gd
+++ game/scenes/poc/scripts/vision_cone.gd
@@ -5,10 +5,20 @@ enum DIRECTION {
 	COUNTER_CLOCKWISE = -1
 }
 
+var MIN_ROTATION_THRESHOLD = -45
+var MAX_ROTATION_THRESHOLD = 45
+
 var scan_speed:float = 30
 var scan_direction = DIRECTION.CLOCKWISE
-# var max_rotation_threshold = 
+
+func _ready():
+	rotation_degrees = MIN_ROTATION_THRESHOLD
+
 
 func _physics_process(delta):
 	rotation_degrees += scan_speed * scan_direction * delta
 	
+	if rotation_degrees >= MAX_ROTATION_THRESHOLD:
+		scan_direction = DIRECTION.COUNTER_CLOCKWISE
+	elif rotation_degrees <= MIN_ROTATION_THRESHOLD:
+		scan_direction = DIRECTION.CLOCKWISE
