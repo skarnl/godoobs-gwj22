f80e8c1b7f7db67cb5fd5863f57f015d12460ad2
Oskar van Velden <oskar@rakso.nl>
2020-06-13 22:53:35 +0200

    Rotate vision cone using AnimationPlayer

--- game/scripts/enemies/enemy.gd
+++ game/scripts/enemies/enemy.gd
@@ -9,7 +9,8 @@ enum LookDirection {
 	DOWN
 }
 
-onready var vision_cone = $LookRotation
+onready var look_rotation = $LookRotation
+onready var sweep_animator = $LookRotation/AnimationPlayer
 onready var player = get_parent().get_parent().get_node("player")
 var motion = Vector2()
 
@@ -19,34 +20,52 @@ export(LookDirection) var look_direction = LookDirection.RIGHT
 
 func _ready():
 	set_physics_process(false)
+
 	match look_direction:
 		LookDirection.LEFT:
-			vision_cone.rotation_degrees = 180
+			look_rotation.rotation_degrees = 180
 
 		LookDirection.UP:
-			vision_cone.rotation_degrees = -90
+			look_rotation.rotation_degrees = -90
 
 		LookDirection.RIGHT:
-			vision_cone.rotation_degrees = 0
+			look_rotation.rotation_degrees = 0
 
 		LookDirection.DOWN:
-			vision_cone.rotation_degrees = 90
+			look_rotation.rotation_degrees = 90
 
+	start_looking()
+	
 
 func _on_vision_cone_area_entered(area):
 	if area.name == "detection_area":
 		emit_signal("player_detected")
 		set_physics_process(true)
-
+	
 func _physics_process(delta):
 	motion = (player.get_global_position() - self.global_position)
 	motion /= motion.length()
 	motion *= MOVEMENT_SPEED
 	move_and_slide(motion)
-
+	
+	stop_looking()
+	focus_on_player()
 
 func _on_vision_cone_area_exited(area):
 	if area.name == "detection_area":
 		print("player escaped")
 		motion = 0
 		set_physics_process(false)
+		start_looking()
+
+
+func stop_looking():
+	sweep_animator.stop()
+
+
+func start_looking():
+	sweep_animator.play()
+
+
+func focus_on_player():
+	look_rotation.get_node("PivotPoint").look_at(player.position)
