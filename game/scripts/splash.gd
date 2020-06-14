3c954227fed9c977c2ea47ee050385ad07237ad8
Oskar van Velden <oskar@rakso.nl>
2020-06-14 23:57:53 +0200

    Temporary skip splash screen

--- game/scripts/splash.gd
+++ game/scripts/splash.gd
@@ -5,6 +5,8 @@ func _ready():
 	$AnimationPlayer.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
 	$AnimationPlayer.play("splash_intro")
 
+	_on_AnimationPlayer_animation_finished("skip it");
+
 
 func _on_AnimationPlayer_animation_finished(animation_name):
 	SceneLoader.goto_scene("res://scenes/main.tscn")
