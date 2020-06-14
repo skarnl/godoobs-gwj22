c7aa581d5aa22b03de8075f851e3ddc29bc4073e
Oskar van Velden <oskar@rakso.nl>
2020-06-14 23:55:26 +0200

    Add tail animation

--- game/scripts/movable_nodes/Player.gd
+++ game/scripts/movable_nodes/Player.gd
@@ -9,6 +9,9 @@ onready var animation_player = $AnimationPlayer
 onready var animation_tree:AnimationTree = $AnimationTree
 onready var animation_state:AnimationNodeStateMachine = animation_tree.get("parameters/playback")
 
+onready var tail = $TailAnimation
+var tail_direction = Vector2.LEFT
+
 func _ready():
 	interactions.connect("progress_changed",self,"change_progress_bar")
 	interactions.progress=50
@@ -27,6 +30,8 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Walking/blend_position", input_vector)
+		tail_direction = input_vector
+
 		animation_state.travel("Walking")
 		
 		motion = input_vector * speed
@@ -36,6 +41,7 @@ func _physics_process(delta):
 		motion = Vector2.ZERO
 
 	move_and_slide(motion)
+	set_tail_position()
 
 func destroy():
 	queue_free()
@@ -53,3 +59,31 @@ func change_progress_bar(new_value:float):
 func register_with_all_enemies():
 	for enemy in get_tree().get_nodes_in_group("enemies"):
 		enemy.set_player_reference(self)
+
+
+func set_tail_position():
+	var position = Vector2.ZERO
+	var rotation = 0
+	
+	#left
+	if tail_direction.x < 0:
+		position = Vector2(23,0)
+		rotation = -90
+	
+	#right
+	elif tail_direction.x > 0:
+		position = Vector2(-23,0)
+		rotation = 90
+	
+	#bottom
+	elif tail_direction.y < 0:
+		position = Vector2(0, 23)
+		rotation = 0
+
+	#top
+	elif  tail_direction.y > 0:
+		position = Vector2(0, -23)
+		rotation = -180
+
+	tail.rotation_degrees = rotation
+	tail.position = position
