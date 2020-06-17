4951945d7418b7fc8269a347bc56fc6d642478a9
Oskar van Velden <oskar@rakso.nl>
2020-06-17 02:14:00 +0200

    Add option to deliver item

--- game/global/inventory.gd
+++ game/global/inventory.gd
@@ -12,7 +12,8 @@ func _ready():
 		items[index] = { 
 			"id": index,
 			"name": str(ItemTypes.types.keys()[index]),
-			"picked_up": false
+			"picked_up": false,
+			"delivered": false
 		}
 
 
@@ -22,7 +23,7 @@ func get_items() -> Dictionary:
 
 func get_items_picked_up() -> Array:
 	var picked_up_items = []
-	for item in items:
+	for item in items.values():
 		if item.picked_up:
 			picked_up_items.append(item)
 	
@@ -34,11 +35,33 @@ func pickup(item_index) -> void:
 
 	print("Item [%s] picked up" % items[item_index].name)
 	
-	mark_item_picked_up(item_index)
+	items[item_index].picked_up = true
 	
 	emit_signal('items_changed')
 	
 	
-func mark_item_picked_up(item_index) -> void:
-	items[item_index].picked_up = true
+func is_item_picked_up(name: String) -> bool:
+	for item in items.values():
+		if item.name == name:
+			return item.picked_up
+
+	print('ERROR: Item %s cant be found in the Inventory' % name)
+	return false
+
+
+func deliver(item_name: String) -> void:
+	var item_index = _get_item_index_by_name(item_name)
 	
+	if item_index != -1:
+		print("Item [%s] delivered" % items[item_index].name)
+		items[item_index].delivered = true
+			
+	emit_signal('items_changed')
+
+
+func _get_item_index_by_name(name: String) -> int:
+	for item_index in items:
+		if items[item_index].name == name:
+			return item_index
+			
+	return -1
