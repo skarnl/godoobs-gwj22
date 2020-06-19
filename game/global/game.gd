9c74ad0ca06ed1b86b3298433a4f07cc725985da
Oskar van Velden <oskar@rakso.nl>
2020-06-20 00:08:16 +0200

    Add pause state

--- game/global/game.gd
+++ game/global/game.gd
@@ -2,6 +2,23 @@
 
 extends Node
 
+signal game_paused
+signal game_resumed
+
+
+enum GameState {
+	SPLASH,
+	MENU,
+	GAME,
+	PAUSED,
+	GAME_OVER	
+}
+
+# TODO we need to keep track of the global application state
+# so we can act depending on what state we are in (State Machine pattern)
+
+var _current_state: int = GameState.GAME
+
 var QuestState = {
 	"NOT_ACTIVE": "not_active",
 	"ACTIVE": "active",
@@ -9,6 +26,11 @@ var QuestState = {
 }
 
 var quest_states = {}
+
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
 
 func get_quest_state(quest_id: String) -> String:
 	if quest_states.has(quest_id):
@@ -20,19 +42,57 @@ func set_quest_state(quest_id: String, state: String) -> void:
 	assert(state in QuestState.values())
 	quest_states[quest_id] = state
 
-# TODO we need to keep track of the global application state
-# so we can act depending on what state we are in (State Machine pattern)
-
-#enum State {
-#	SPLASH,
-#	PAUSED,
-#	MAIN_MENU,
-#	MAP,
-#	GAME
-#}
-#
-#var _current_state: State = State.GAME setget _set_current_state
-
 # change the state to the next
-#func transition_to(new_state: State) -> void:
-	
+func transition_to(new_state: int) -> void:
+	match _current_state:
+		GameState.SPLASH:
+			if new_state == GameState.MENU:
+				_current_state = GameState.MENU
+				SceneLoader.goto_scene('res://menu/main_menu.tscn')
+		
+		GameState.MENU:
+			if new_state == GameState.GAME:
+				_current_state = GameState.GAME
+				SceneLoader.goto_scene('res://levels/level_01.tscn')
+				
+		GameState.GAME:
+			if new_state == GameState.PAUSED:
+				_current_state = GameState.PAUSED
+				_handle_paused(true)
+			
+			if new_state == GameState.GAME_OVER:
+				_current_state = GameState.GAME_OVER
+				SceneLoader.goto_scene('res://ui/game_over.tscn')
+		
+		GameState.GAME_OVER:
+			if new_state == GameState.MENU:
+				_current_state = GameState.MENU
+				SceneLoader.goto_scene('res://menu/main_menu.tscn')
+				
+		GameState.PAUSED:
+			if new_state == GameState.MENU:
+				get_tree().paused = false
+				_current_state = GameState.MENU
+				SceneLoader.goto_scene('res://menu/main_menu.tscn')
+				
+			if new_state == GameState.GAME:
+				_current_state = GameState.GAME
+				_handle_paused(false)
+
+
+func _handle_paused(paused: bool):
+	if paused:
+		emit_signal("game_paused")
+		get_tree().paused = true
+	else:
+		emit_signal("game_resumed")
+		get_tree().paused = false
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			if _current_state == GameState.GAME:
+				transition_to(GameState.PAUSED)
+			elif _current_state == GameState.PAUSED:
+				transition_to(GameState.GAME)
--- game/gui/pause_menu.gd
+++ game/gui/pause_menu.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 func _ready():
 	var pos1=$CanvasLayer/Button.get_global_position()
@@ -7,25 +7,31 @@ func _ready():
 	$CanvasLayer/Button.set_global_position(pos1)
 	$CanvasLayer/Button2.set_as_toplevel(true)
 	$CanvasLayer/Button2.set_global_position(pos2)
+	
+	Game.connect("game_paused", self, "_on_Game_game_paused")
+	Game.connect("game_resumed", self, "_on_Game_game_resumed")
+	
+	_toggle(false)
+
 
 func _on_Button_button_up():
-	get_tree().paused=false
-	$CanvasLayer/Button.visible = false
-	$CanvasLayer/Button2.visible = false
-	$CanvasLayer/ColorRect.visible = false
+	Game.transition_to(Game.GameState.GAME)
 
 
 func _on_Button2_button_up():
-	get_tree().paused = false
-	$CanvasLayer/Button.visible = false
-	$CanvasLayer/Button2.visible = false
-	$CanvasLayer/ColorRect.visible = false
-	get_parent().get_parent()._on_player_area_lost()
+	Game.transition_to(Game.GameState.MENU)
+	
 
-func _input(event):
-	if event is InputEventKey:
-		if event.pressed and event.scancode == KEY_ESCAPE:
-			$CanvasLayer/Button.visible = true
-			$CanvasLayer/Button2.visible = true
-			$CanvasLayer/ColorRect.visible = true
-			get_tree().paused = true
+func _on_Game_game_paused():
+	_toggle(true)
+	
+
+func _on_Game_game_resumed():
+	_toggle(false)
+
+
+func _toggle(show: bool):
+	$CanvasLayer/Label.visible = show
+	$CanvasLayer/Button.visible = show
+	$CanvasLayer/Button2.visible = show
+	$CanvasLayer/ColorRect.visible = show
