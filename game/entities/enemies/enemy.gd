153bfc9f3c8628d2d0065512cd896d7a999c1a47
Oskar van Velden <oskar@rakso.nl>
2020-06-20 21:03:07 +0200

    Add enemy movement

--- game/entities/enemies/enemy.gd
+++ game/entities/enemies/enemy.gd
@@ -10,33 +10,19 @@ enum LookDirection {
 }
 
 onready var look_rotation = $LookRotation
-onready var sweep_animator = $LookRotation/AnimationPlayer
+onready var body_animator = $AnimationPlayer
+
 var player:Node2D
 var is_player_seen:=false
 var motion = Vector2()
-export var MOVEMENT_SPEED = 50
+export var MOVEMENT_SPEED = 100
 export var detection_speed=1000 #how fast does the enemy reduce the progress
 export var value=10 #how much does killing this enemy cost for the progress
 var x:float
-export(LookDirection) var look_direction = LookDirection.RIGHT
 
 func _ready():
 	set_physics_process(false)
-
-	match look_direction:
-		LookDirection.LEFT:
-			look_rotation.rotation_degrees = 180
-
-		LookDirection.UP:
-			look_rotation.rotation_degrees = -90
-
-		LookDirection.RIGHT:
-			look_rotation.rotation_degrees = 0
-
-		LookDirection.DOWN:
-			look_rotation.rotation_degrees = 90
-
-	start_looking()
+	body_animator.play("idle_right")
 	
 
 func _on_vision_cone_area_entered(area):
@@ -45,6 +31,7 @@ func _on_vision_cone_area_entered(area):
 		is_player_seen=true
 		x=interactions.progress
 		set_physics_process(true)
+		body_animator.play("walk_right")
 	
 func _physics_process(delta):
 	motion = (player.get_global_position() - self.global_position)
@@ -52,7 +39,6 @@ func _physics_process(delta):
 	motion /= motion.length()
 	motion *= MOVEMENT_SPEED
 	move_and_slide(motion)
-	stop_looking()
 	focus_on_player()
 	interactions.progress=x
 
@@ -61,22 +47,14 @@ func _on_vision_cone_area_exited(area):
 		motion = 0
 		is_player_seen=false
 		set_physics_process(false)
-		start_looking()
-
-
-func stop_looking():
-	sweep_animator.stop()
-
-
-func start_looking():
-	sweep_animator.play()
+		body_animator.play("idle_right")
 
 
 func focus_on_player():
 	if not player:
 		return
 		
-	look_rotation.get_node("PivotPoint").look_at(player.position)
+	look_at(player.position)
 
 func destroy():
 	interactions.progress-=value
@@ -84,6 +62,8 @@ func destroy():
 	queue_free()
 
 func _on_player_detector_area_entered(area):
+	#TODO trigger game-over state
+	
 	if area.name=="detection_area":
 		if is_player_seen:
 			area.get_parent().destroy()
