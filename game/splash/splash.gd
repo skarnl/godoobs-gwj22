a6b5f98efcf2e327ce5dfbb3390eada9f76b554f
Oskar van Velden <oskar@rakso.nl>
2020-06-21 14:29:23 +0200

    Goto main-menu after splash

--- game/splash/splash.gd
+++ game/splash/splash.gd
@@ -16,4 +16,4 @@ func _ready():
 
 
 func _on_AnimationPlayer_animation_finished(animation_name):
-	SceneLoader.goto_scene("res://levels/level_01.tscn")
+	SceneLoader.goto_scene("res://menu/main_menu.tscn")
