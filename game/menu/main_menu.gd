310b4087801631df063265f94d343d5e1162fa7a
Oskar van Velden <oskar@rakso.nl>
2020-06-21 21:14:57 +0200

    Remove quit button from main-menu

--- game/menu/main_menu.gd
+++ game/menu/main_menu.gd
@@ -16,7 +16,6 @@ func _ready():
 	
 	$Panel/start_button.connect("pressed", self, "_on_start_button_pressed")
 	$Panel/credits_button.connect("pressed", self, "_on_credits_button_pressed")
-	$Panel/quit_button.connect("pressed", self, "_on_quit_button_pressed")
 	credits_close_button.connect("pressed", self, "_on_credits_close_button_pressed")
 	
 	$Panel/start_button.grab_focus()
@@ -37,8 +36,3 @@ func _on_credits_button_pressed():
 func _on_credits_close_button_pressed():
 	credits.hide()
 	$Panel/credits_button.grab_focus()
-	
-	
-func _on_quit_button_pressed():
-	get_tree().quit()
-
